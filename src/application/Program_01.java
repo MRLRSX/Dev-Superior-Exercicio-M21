62f08a60de1f7b6e92266f0320716caf7b507f54
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-21 17:46:47 -0300

    INSERT

--- src/application/Program_01.java
+++ src/application/Program_01.java
@@ -2,11 +2,12 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import db.ConexaoDB;
 import model.Seller;
@@ -14,14 +15,14 @@ import model.Seller;
 public class Program_01 {
     
 	
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException, SQLException {
 	  Connection conn = null;
 	  PreparedStatement st = null;
 	  SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	  Seller seller = new Seller(null, "Anna Clara", "acbr@grupoacbr.com.br", LocalDateTime.now(), 125000.00, 1);
+	  Seller seller = new Seller(null, "Vicente Rocha", "vicenterochajp@gmail.com.br", LocalDateTime.now(), 5000.00, 1);
 	  try {
 		  conn = ConexaoDB.getConnection();
-		  st = conn.prepareStatement("INSERT INTO seller (name, email, birthdate, basesalary, departmentid) VALUES (?, ?, ?, ?, ?)");
+		  st = conn.prepareStatement("INSERT INTO seller (name, email, birthdate, basesalary, departmentid) VALUES (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 		  
 		  st.setString(1, seller.getName());
 		  st.setString(2, seller.getEmail());
@@ -29,12 +30,22 @@ public class Program_01 {
 		  st.setDouble(4, seller.getBaseSalary());
 		  st.setInt(5, seller.getDepartmentId());
 		  int rowsAffected = st.executeUpdate();
-		  System.out.println(rowsAffected);
+		  
+		  if(rowsAffected > 0) {
+			  ResultSet rs = st.getGeneratedKeys();
+			  while(rs.next()) {
+				  int id = rs.getInt(1);
+				  System.out.println("Done! id: "+ id);
+			  }
+		  }else {
+			  System.out.println("No rown affected");			  
+		  }
+		  
 	  }catch(SQLException error){
 		  error.printStackTrace();
 	  }finally {
 		  ConexaoDB.closeStatement(st);
-		 conn.close();
+		  conn.close();
 	  }
 	}
 }
